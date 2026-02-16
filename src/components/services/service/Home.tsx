aef06cb5adf4d19dba57fedd649ab7019ebb2a37
Jerry Malloney <jerrymalloney@gmail.com>
2026-02-16 17:38:17 -0400

    feat: enhance services page with dynamic Hero and FAQ sections

--- src/components/services/service/Home.tsx
+++ src/components/services/service/Home.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
 
-const Hero = () => {
+const Hero = ({
+  content,
+  imgSrc,
+  imgAlt,
+}: {
+  content: React.ReactNode;
+  imgSrc: string;
+  imgAlt: string;
+}) => {
   return (
     <div className="relative">
       <Image
-        alt=""
+        alt={imgAlt}
         fill
-        src={"/hero2.jpg"}
+        src={imgSrc}
         className="object-cover object-[25%_40%]"
       />
       <div className="relative py-[40%] md:py-[30%] lg:py-[25%] xl:py-[10%] bg-black/35 px-4">
         <div className="container mx-auto">
           <div className="max-w-xl ml-auto">
             <div className="space-y-4 border-l-6 border-brand-primary  text-white p-6">
-              <h1 className="text-4xl  md:text-4xl font-semibold ">
-                Lorem ipsum dolor sit amet,Lorem ipsum dolor sit
-              </h1>
-              <p className=" text-lg ">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-                vel quam venenatis, posuere turpis vitae, aliquam nisi.
-              </p>
+              {content}
             </div>
           </div>
         </div>
